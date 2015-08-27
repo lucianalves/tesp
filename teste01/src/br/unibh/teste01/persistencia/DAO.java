5fe7820742b1e98f4babd0a32a4b6912d3f89be3
Luciana <Luciana@10.13.10.229>
2015-08-26 21:33:51 -0300

    Projetoteste

---
+++ teste01/src/br/unibh/teste01/persistencia/DAO.java
@@ -0,0 +1,14 @@
+package br.unibh.teste01.persistencia;
+
+import java.util.List;
+
+public interface DAO <T,K> {
+	
+	public T find(K id);
+	public void insert(T t);
+	public void update(T t);
+	public void delete(T t);
+	public List<T> findAll();
+
+		
+}
