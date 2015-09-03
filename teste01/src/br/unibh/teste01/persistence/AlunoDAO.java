be9d73b55aa8e07b7588203bd150248973d34718
Luciana <Luciana@10.206.97.63>
2015-09-02 22:18:34 -0300

    Criando os metodos delete, insert e update no alunoDAO, e no teste java

--- teste01/src/br/unibh/teste01/persistence/AlunoDAO.java
+++ teste01/src/br/unibh/teste01/persistence/AlunoDAO.java
@@ -10,140 +10,149 @@ import br.unibh.teste01.entidades.Aluno;
 import br.unibh.teste01.persistencia.DAO;
 import br.unibh.teste01.persistencia.JDBCUtil;
 
-public class AlunoDAO implements DAO<Aluno, Long>{
+public class AlunoDAO implements DAO<Aluno, Long> {
 
-	
-	private static SimpleDateFormat df =
-			new SimpleDateFormat("yyyy-MM-dd");
-	
+	private static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
 	@Override
 	public Aluno find(Long id) {
 		try {
-			
-			PreparedStatement p = JDBCUtil.getConnection().prepareStatement(
-					"select * from tb_aluno where id = ?");
-			
+
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("select * from tb_aluno where id = ?");
+
 			p.setLong(1, id);
 			ResultSet res = p.executeQuery();
-			
+
 			if (res.next()) {
-				return new Aluno(
-								res.getLong("id"),
-								res.getString("nome"),
-								res.getString("cpf"),
-								res.getString("matricula"),
-								res.getDate("data_Aniversario"));
-		
+				return new Aluno(res.getLong("id"), res.getString("nome"), res.getString("cpf"),
+						res.getString("matricula"), res.getDate("data_Aniversario"));
+
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		
+
 		} finally {
-			
+
 			JDBCUtil.closeConnection();
 		}
 		return null;
 	}
 
-	
 	public Aluno find(String nome) {
 		try {
-			
-			PreparedStatement p = JDBCUtil.getConnection().prepareStatement(
-					"select * from tb_aluno where nome like ?");
-			
-			p.setString(1,nome+"%");
+
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("select * from tb_aluno where nome like ?");
+
+			p.setString(1, nome + "%");
 			ResultSet res = p.executeQuery();
-			
+
 			if (res.next()) {
-				return new Aluno(
-								res.getLong("id"),
-								res.getString("nome"),
-								res.getString("cpf"),
-								res.getString("matricula"),
-								res.getDate("data_Aniversario"));
-		
+				return new Aluno(res.getLong("id"), res.getString("nome"), res.getString("cpf"),
+						res.getString("matricula"), res.getDate("data_Aniversario"));
+
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		
+
 		} finally {
-			
+
 			JDBCUtil.closeConnection();
 		}
 		return null;
 	}
+
 	@Override
 	public void insert(Aluno t) {
 		try {
-			
+
 			PreparedStatement p = JDBCUtil.getConnection().prepareStatement(
-					"insert into tb_aluno (nome, cpf, matricula, data_aniversario" + 
-					"values (?,?,?,?,?)");
-			
+					"insert into tb_aluno (nome, cpf, matricula, data_aniversario" + "values (?,?,?,?,?)");
+
 			p.setString(1, t.getNome());
 			p.setString(2, t.getCpf());
 			p.setString(3, t.getMatricula());
-			p.setString(4, df.format(t.getDataAniversario()));	
-			p.executeUpdate();	
-				
+			p.setString(4, df.format(t.getDataAniversario()));
+			p.executeUpdate();
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		
+
 		} finally {
-			
+
 			JDBCUtil.closeConnection();
 		}
-		
-	}
-		
 
-	@Override
-	public void update(Aluno t) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
+
 	public void delete(Aluno t) {
-		// TODO Auto-generated method stub
-		
+
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("delete from tb_aluno where id = ?");
+
+			p.setLong(1, t.getId());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+
+			JDBCUtil.closeConnection();
+		}
+
+	}
+
+	@Override
+
+	public void update(Aluno t) {
+		try {
+
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement(
+					"Update tb_aluno set nome = ?, cpf =?, matricula = ?, data_aniversario = ?" + "where id =?");
+
+			p.setString(1, t.getNome());
+			p.setString(2, t.getCpf());
+			p.setString(3, t.getMatricula());
+			p.setString(4, df.format(t.getDataAniversario()));
+			p.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+
+			JDBCUtil.closeConnection();
+		}
+
 	}
 
 	@Override
 	public List<Aluno> findAll() {
 		ArrayList<Aluno> lista = new ArrayList<Aluno>();
 		try {
-			
-			ResultSet res = JDBCUtil.getConnection().prepareStatement(
-					"select * from tb_aluno").executeQuery();
+
+			ResultSet res = JDBCUtil.getConnection().prepareStatement("select * from tb_aluno").executeQuery();
 			while (res.next()) {
-				
-				lista.add(
-						new Aluno (
-								res.getLong("id"),
-								res.getString("nome"),
-								res.getString("cpf"),
-								res.getString("matricula"),
-								res.getDate("data_Aniversario"))
-								
-						);
-		
+
+				lista.add(new Aluno(res.getLong("id"), res.getString("nome"), res.getString("cpf"),
+						res.getString("matricula"), res.getDate("data_Aniversario"))
+
+				);
+
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		
+
 		} finally {
-			
+
 			JDBCUtil.closeConnection();
 		}
-		
+
 		return lista;
 	}
-	
-	
 
 }
